e8956fca290453c8ede87571617bc4a47f631d1b
gayashan4lk <gayashan4lk@gmail.com>
2024-03-23 23:14:55 +0530

    first endpoint

---
+++ TodoApi/Persistence/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Persistence;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options): base(options){}
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+using TodoApi.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/todos", async (TodoDb db) => await db.Todos.ToListAsync());
+app.MapPost("/todos", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todos/{todo.Id}", todo);
+});
 
 app.Run();
